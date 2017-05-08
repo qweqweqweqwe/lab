3fc0ec1f7f691bd04ee220c5c92051652b1ad6f0
Nikita Burtsev <burtsev.n23@gmail.com>
2017-05-08 14:41:58 +0300

    task db

--- djangorest/todolist/models.py
+++ djangorest/todolist/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    name = models.CharField(max_length=200, blank=True)
+    description = models.TextField(max_length=1000, blank=True)
+    completed = models.BooleanField(default=False)
+    date_created = models.DateField(auto_now_add=True)
+    due_date = models.DateField(null=True, blank=True)
+    date_modified = models.DateField(auto_now=True)
+
+    PRIORITY = (
+        ('h', 'High'),
+        ('m', 'Medium'),
+        ('l', 'Low'),
+        ('n', 'None')
+    )
+
+    priority = models.CharField(max_length=1, choices=PRIORITY, default='n')
+
+    def __str__(self):
+        return "{}".format(self.name)
