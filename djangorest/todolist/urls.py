e140dd12e120681484f764b119321276238e2d33
Nikita Burtsev <burtsev.n23@gmail.com>
2017-05-24 22:34:50 +0300

    add login/logout

--- djangorest/todolist/urls.py
+++ djangorest/todolist/urls.py
@@ -3,17 +3,13 @@ from rest_framework import routers
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import TasklistCreateView, TasklistDetailsView, TaskCreateView, TaskDetailsView, UserViewSet, CreateUserView
 
-# Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-# router.register(r'users', UserViewSet)
-
-
 urlpatterns = {
     # url(r'^', include(router.urls)),
     url(r'^todolists/$', TasklistCreateView.as_view(), name="lists"),
     url(r'^todolists/(?P<pk>[0-9]+)/$', TasklistDetailsView.as_view(), name="list-detail"),
     url(r'^todolists/(?P<list_id>[0-9]+)/tasks', TaskCreateView.as_view(), name="tasks"),
     url(r'^todolists/(?P<list_id>[0-9]+)/tasks/(?P<pk>[0-9]+)', TaskDetailsView.as_view(), name="task-detail"),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^register/$', CreateUserView.as_view(), name='user')
 }
 
